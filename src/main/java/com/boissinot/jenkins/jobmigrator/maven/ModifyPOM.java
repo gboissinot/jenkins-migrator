aab2b4f19e69d547b21c15172261a3f19d9fd173
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-07-06 22:41:23 +0200

    Refactor

--- src/main/java/com/boissinot/jenkins/jobmigrator/maven/ModifyPOM.java
+++ src/main/java/com/boissinot/jenkins/jobmigrator/maven/ModifyPOM.java
@@ -1,8 +1,12 @@
 package com.boissinot.jenkins.jobmigrator.maven;
 
-import com.boissinot.jenkins.jobmigrator.JobMigrationException;
-import com.sun.org.apache.xml.internal.serialize.OutputFormat;
-import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.springframework.integration.Message;
 import org.springframework.integration.MessageHeaders;
 import org.w3c.dom.Document;
@@ -10,11 +14,9 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.ByteArrayInputStream;
-import java.io.StringWriter;
-import java.io.Writer;
+import com.boissinot.jenkins.jobmigrator.JobMigrationException;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 /**
  * @author Gregory Boissinot
@@ -28,43 +30,31 @@ public class ModifyPOM {
 
         String pomFileContent = pomFileMessage.getPayload();
 
-
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = null;
         try {
             docBuilder = docFactory.newDocumentBuilder();
 
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(pomFileContent.getBytes());
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(pomFileContent.getBytes("UTF-8"));
             InputSource inputSource = new InputSource(byteArrayInputStream);
             Document document = docBuilder.parse(inputSource);
-
-
-            //--Change connection
-            //String previousNodeValue = node.getTextContent();
-            //System.out.println("PreviousValue: " + previousNodeValie);
-            //node.setTextContent("toto");
-
-            //${scm.connection.cvs.tango-ds}:Instrumentation/AcquireWaveformLecroy
-
-            //String newValue =  previousNodeValue.replace("scm.connection.cvs.tango-ds","scm.connection.svn.tango-ds");
 
             String baseSVNURL = "https://svn.code.sf.net/p/tango-ds/code/";
             String newValue = remoteUrl.substring(remoteUrl.lastIndexOf(baseSVNURL) + baseSVNURL.length());
             newValue = "${scm.connection.svn.tango-ds}/" + newValue;
 
-            //Connection element
+            // Connection element
             final NodeList connection = document.getElementsByTagName("connection");
             final Node connectionNode = connection.item(0);
             connectionNode.setTextContent(newValue);
-            //developerConnection element
+            // developerConnection element
             final NodeList developerConnection = document.getElementsByTagName("developerConnection");
             final Node developerConnectionNode = developerConnection.item(0);
             developerConnectionNode.setTextContent(newValue);
-            //url element
+            // url element
             final NodeList url = document.getElementsByTagName("url");
             final Node urlNode = url.item(0);
             urlNode.setTextContent(newValue);
-
 
             Writer out = new StringWriter();
             OutputFormat format = new OutputFormat();
@@ -77,10 +67,8 @@ public class ModifyPOM {
 
             return out.toString();
 
-
         } catch (Exception e) {
             throw new JobMigrationException(e);
         }
-
     }
 }
--- src/main/java/com/boissinot/jenkins/jobmigrator/maven/POMFromSVNURLBuilder.java
+++ src/main/java/com/boissinot/jenkins/jobmigrator/maven/POMFromSVNURLBuilder.java
@@ -1,35 +1,45 @@
 package com.boissinot.jenkins.jobmigrator.maven;
 
-import com.boissinot.jenkins.jobmigrator.JobMigrationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.apache.commons.io.IOUtils;
 import org.springframework.integration.Message;
 import org.springframework.integration.support.MessageBuilder;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.*;
+import com.boissinot.jenkins.jobmigrator.JobMigrationException;
 
 /**
  * @author Gregory Boissinot
  */
 public class POMFromSVNURLBuilder {
 
-
-    //Returns a message with pom file as payload
-    //Takes a message with remote SVN URL content
+    // Returns a message with pom file as payload
+    // Takes a message with remote SVN URL content
     public Message<String> process(Message<String> remoteURLMessage) {
 
         String remoteURL = remoteURLMessage.getPayload();
-        String pomFileContent;
-        try {
-            String pomURL = remoteURL + "pom.xml";
 
-                 Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.synchrotron-soleil.fr", 8080));
+        String pomFileContent;
+
+        String pomURL;
+        if (remoteURL.endsWith("/"))
+            pomURL = remoteURL + "pom.xml";
+        else
+            pomURL = remoteURL + "/pom.xml";
+
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.synchrotron-soleil.fr", 8080));
             pomURL = pomURL.replace("https", "http");
             System.out.println("Fetching " + pomURL);
 
             URL url = new URL(pomURL);
-            final URLConnection urlConnection = url.openConnection(proxy);
+            URLConnection urlConnection = url.openConnection(proxy);
             final InputStream inputStream = urlConnection.getInputStream();
             pomFileContent = IOUtils.toString(inputStream, "UTF-8");
             inputStream.close();
@@ -40,11 +50,7 @@ public class POMFromSVNURLBuilder {
             throw new JobMigrationException(e);
         }
 
-
-        return MessageBuilder
-                .withPayload(pomFileContent)
-                .copyHeaders(remoteURLMessage.getHeaders())
-                .setHeader("REMOTE_URL", remoteURL)
-                .build();
+        return MessageBuilder.withPayload(pomFileContent).copyHeaders(remoteURLMessage.getHeaders())
+                .setHeader("REMOTE_URL", remoteURL).build();
     }
 }
